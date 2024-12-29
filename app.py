206a607f7bfc0609f2411a19d4d7015f7d8e7cc4
Sasha2810 <95037701+Sasha2810@users.noreply.github.com>
2024-12-30 01:28:58 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_session import Session
-import uuid
 import os
-import redis
 
 app = Flask(__name__)
 
-# Настройка Flask-Session
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_KEY_PREFIX'] = 'cart_session:'
-app.config['SESSION_REDIS'] = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-Session(app)
-app = Flask(__name__)
-
-# secret_key = os.urandom(24)  
+# Секретный ключ для сессий
 app.secret_key = 'aP9#fN4@8TgL!kE1$2ZxVpRqSdUoWmYq'
 
+# Настройки для загрузки изображений
 app.config['UPLOAD_FOLDER'] = 'static/image'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
@@ -32,10 +20,8 @@ archived_products = []
 # Главная страница
 @app.route('/')
 def index():
-    for img in products[0]['images']:
-        print(url_for('static', filename='image/' + img))
-
     return render_template('index.html', products=products, archived_products=archived_products)
+
 # Админка
 @app.route('/QW1F23V4', methods=['GET', 'POST'])
 def admin_panel():
@@ -57,7 +43,7 @@ def admin_panel():
         print(products)
         return redirect(url_for('admin_panel'))
 
-    return render_template('admin.html', products=products,  archived_products=archived_products,)
+    return render_template('admin.html', products=products, archived_products=archived_products)
 
 @app.route('/delete_from_catalog/<int:product_id>', methods=['POST'])
 def delete_from_catalog(product_id):
@@ -71,15 +57,13 @@ def delete_from_archive(product_id):
         archived_products.pop(product_id)
     return redirect(url_for('admin_panel'))
 
-
 @app.route('/archive/<int:product_id>', methods=['POST'])
 def archive_product(product_id):
     if 0 <= product_id < len(products):
-        print(products)
         archived_products.append(products.pop(product_id))
-        print(archived_products)
     return redirect(url_for('admin_panel'))
 
+# Обновленный маршрут для добавления товаров в корзину
 @app.route('/add_to_cart/<int:product_id>', methods=['POST'])
 def add_to_cart(product_id):
     if 'cart' not in session:
@@ -100,18 +84,15 @@ def add_to_cart(product_id):
 
     return redirect(url_for('index'))
 
-
+# Обновленный маршрут для удаления товара из корзины
 @app.route('/remove_from_cart/<int:product_id>', methods=['POST'])
 def remove_from_cart(product_id):
     if 'cart' in session:
-        print("Cart before removal:", session['cart'])  # Для отладки
-        # Удаляем элемент по product_id
         session['cart'] = [item for item in session['cart'] if item['id'] != product_id]
         session.modified = True  # Указываем, что сессия была изменена
-        print("Cart after removal:", session['cart'])  # Для отладки
+        print(session['cart'])  # Для отладки
+
     return redirect(url_for('index'))
-
-
 
 @app.route('/checkout', methods=['GET', 'POST'])
 def checkout():
@@ -120,12 +101,8 @@ def checkout():
 
     total = sum(item['price'] for item in session['cart'])  # Суммируем цены товаров из сессии
     print(total)
-    
+
     return render_template('checkout.html', cart=session['cart'], total=total)
-
-
-    
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=False)
